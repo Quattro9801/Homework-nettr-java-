547119def28402655aebbb894e854c267a71730b
Ivan Netreba <73758348+Quattro9801@users.noreply.github.com>
2021-04-05 10:23:56 +0300

    Delete Java/Homework2/Chapter1 directory

--- Java/Homework2/Chapter1/MaxMinDouble.java
+++
@@ -1,9 +0,0 @@
-package Homework2.exercise4Chapter1;
-
-public class MaxMinDouble {
-    public static void main(String[] args) {
-
-        System.out.println("MinValue ="+Math.nextUp(Double.MIN_VALUE));
-        System.out.println("MaxValue ="+Math.nextUp(Double.MAX_VALUE));//+
-    }
-}
