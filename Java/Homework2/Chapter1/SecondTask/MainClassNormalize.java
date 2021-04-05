651bbaed3d1df305cfd65fc362d63538f7eebc65
Ivan Netreba <73758348+Quattro9801@users.noreply.github.com>
2021-04-05 10:31:56 +0300

    Add files via upload

---
+++ Java/Homework2/Chapter1/SecondTask/MainClassNormalize.java
@@ -0,0 +1,18 @@
+package exercise2Chapter1;
+
+public class MainClassNormalize {
+    public static void main(String[] args) {
+        Normalize normalize=new Normalize(359);
+        System.out.println("Нормализованный угол "+normalize.calculateAngle());
+        System.out.println("Нормализованный угол(floormod) "+normalize.calculateAngleFloorMod());
+        normalize.setAngle(-20);
+        System.out.println("Нормализованный угол "+normalize.calculateAngle());
+        System.out.println("Нормализованный угол(floormod) "+normalize.calculateAngleFloorMod());
+
+
+
+
+    }
+
+
+}
