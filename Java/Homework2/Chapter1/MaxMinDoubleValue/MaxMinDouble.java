e765368f3fc6dc66f3a3c3d26c6ffb68df2bbc0c
Ivan Netreba <73758348+Quattro9801@users.noreply.github.com>
2021-04-09 22:46:20 +0300

    Add files via upload

---
+++ Java/Homework2/Chapter1/MaxMinDoubleValue/MaxMinDouble.java
@@ -0,0 +1,11 @@
+package Homework2.exercise4Chapter1;
+
+public class MaxMinDouble {
+    public static void main(String[] args) {
+        double value = Math.nextUp(Double.MAX_VALUE);
+        System.out.println("Значение = " + value);
+        System.out.println("MAX_VALUE: " + Double.MAX_VALUE);
+        System.out.println("MIN_VALUE: " + Double.MIN_VALUE);
+
+    }
+}
