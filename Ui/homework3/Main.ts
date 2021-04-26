d768d064591579043c8ac525038e5a77b0622dbc
Ivan Netreba <73758348+Quattro9801@users.noreply.github.com>
2021-04-26 23:29:52 +0300

    Add files via upload

---
+++ Ui/homework3/Main.ts
@@ -0,0 +1,21 @@
+import { LinkedList } from "./LinkedList";
+const linkedList = new LinkedList<String>();
+const linkedList1 = new LinkedList<Number>();
+/* tslint:disable no-var-requires */
+// tslint:disable-next-line:no-require-imports
+const readlineSync = require("readline-sync");
+const n = readlineSync.questionInt("How many elements do you want to add ?");
+const arr_names: number[] = new Array(n);
+const arr_names1: string[] = new Array(n);
+for (let i = 0; i < arr_names1.length; i++) {
+    arr_names1[i] = readlineSync.prompt("Elem =" + i);
+    linkedList.add(arr_names1[i]);
+}
+console.log("Before");
+linkedList.printAll();
+linkedList.showNumber(3);
+linkedList.remove(0);
+console.log("------------------------------");
+console.log("After delete");
+linkedList.printAll();
+
